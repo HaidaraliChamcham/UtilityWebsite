8000cfb9c96f0b901da1dbd92556e97b296c5270
HaidaraliChamcham <haideralichamcham98@gmail.com>
2022-05-21 23:29:55 +0530

    textarea going to be used

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -5,11 +5,11 @@ export default function Layout(props:any) {
 
     return (
         <Grid container xs={12} style={{justifyContent:"space-between"}}>
-            <Grid item xs={4}>
+            <Grid item xs={5}>
                 <textarea style={{height:"300px", width:"100%"}}>{props.code}</textarea>
             </Grid>
 
-            <Grid item xs={7}>
+            <Grid item xs={6}>
                 {props.ui}
             </Grid>
         </Grid>
